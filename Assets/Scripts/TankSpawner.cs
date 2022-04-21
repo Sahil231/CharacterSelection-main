ebd0020b27db66ea60839a2945254828b3e82acb
Sahil231 <sahilmalhotra231@gmail.com>
2022-04-21 06:53:41 +0530

    Initial commit

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    public GameObject tankPrefab;
+    public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject tank = Instantiate(tankPrefab, this.transform.position, Quaternion.identity);
+        camera.transform.parent=tank.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
